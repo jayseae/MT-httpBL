f3146f2970b8374fe4479da8f1ee97519d3fcd72
Chad Everett <github@jayseae.cxliv.org>
2009-07-17 11:07:30 -0400

    Update license to Artistic - 0.0.3

--- trunk/plugins/httpBL/httpBL.pl
+++ trunk/plugins/httpBL/httpBL.pl
@@ -1,5 +1,17 @@
 # ===========================================================================
-# Copyright Everitz Consulting.  Not for redistribution.
+# A Movable Type anti-spam plugin for http:BL.
+# Copyright 2008 Everitz Consulting <everitz.com>.
+#
+# This program is free software:  You may redistribute it and/or modify it
+# it under the terms of the Artistic License version 2 as published by the
+# Open Source Initiative.
+#
+# This program is distributed in the hope that it will be useful but does
+# NOT INCLUDE ANY WARRANTY; Without even the implied warranty of FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+# You should have received a copy of the Artistic License with this program.
+# If not, see <http://www.opensource.org/licenses/artistic-license-2.0.php>.
 # ===========================================================================
 package MT::Plugin::HTTPBL;
 
@@ -22,7 +34,7 @@ my $plugin = MT::Plugin::HTTPBL->new({
     plugin_link    => 'http://everitz.com/mt/httpbl/index.php',
     doc_link       => 'http://everitz.com/mt/httpbl/index.php#install',
 #    l10n_class     => 'HTTPBL::L10N',
-    version        => '0.0.2',
+    version        => '0.0.3',
 #
 # settings
 #
